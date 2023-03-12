8e782dc9bfc1a81aa8d47fffd01e5f8b58e8f754
Azhar1297 <124858214+Azhar1297@users.noreply.github.com>
2023-03-12 23:04:49 +0530

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "refrg" {
+    default = "myvmrg"
+}
+variable "refloc" {
+  default = "centralindia"
+}
+variable "refvnet" {
+    default = "myvmvnet"
+}
+variable "refaddrespace" {
+    default = ["10.8.0.0/16"]
+}
+variable "refsubnet" {
+    default = "myvmsubnet"
+  
+}
